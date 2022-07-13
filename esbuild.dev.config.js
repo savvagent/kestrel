d1601513b46e755427c65ecd652292df6dcef6b4
Rob Hicks <rob@hixfamily.org>
2022-07-12 20:05:47 -0600

    working

---
+++ esbuild.dev.config.js
@@ -0,0 +1,50 @@
+import { build } from 'esbuild'
+import { resolve } from 'path'
+
+const browserConfig = {
+  bundle: true,
+  entryPoints: [resolve('src', 'index.js')],
+  format: 'esm',
+  minify: true,
+  sourcemap: true,
+  outfile: resolve('dist', 'kestrel.js'),
+  watch: true
+}
+
+const cjsConfig = {
+  ...browserConfig,
+  ...{
+    format: 'cjs',
+    minify: false,
+    sourcemap: false,
+    outfile: 'index.js',
+    target: ['node14.0'],
+    watch: true
+  }
+}
+
+const mjsConfig = {
+  ...cjsConfig,
+  ...{
+    format: 'esm',
+    outfile: 'index.mjs'
+  }
+}
+
+const testConfig = {
+  bundle: true,
+  entryPoints: [resolve('test', 'browser.js')],
+  format: 'esm',
+  minify: false,
+  outfile: resolve('test', 'browser-bundle.js'),
+  sourcemap: false,
+  watch: true
+}
+
+Promise.all([
+  build(browserConfig),
+  build(cjsConfig),
+  build(mjsConfig),
+  build(testConfig)
+])
+  .catch((err) => console.log('err', err))
